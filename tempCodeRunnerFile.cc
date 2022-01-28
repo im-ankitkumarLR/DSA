8b1531b16e1a140636834ba544e2909eac1349b1
aknasa <aknasa12@gmail.com>
2022-01-28 13:12:06 +0530

    firstcommit

---
+++ tempCodeRunnerFile.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+//  printf("Hello world\n");
+  printf("Hello India\n");
+
+int i,j,n,a=1;
+cout<<"Entern the number\n";
+cin>>n;
+
+for ( i=1;i <=1;)
+{
+    for ( j = 1; j <=n; j++)
+    {
+        cout<<a<<" ";
+        a=a+1;
+    }
+    
+    cout<<endl;
+}
+
+
+
+
+}
