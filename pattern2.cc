a8cda2b133f817015695572a35ac252d800ea634
aknasa <aknasa12@gmail.com>
2022-01-29 11:08:39 +0530

    Done

--- pattern2.cc
+++ pattern2.cc
@@ -2,17 +2,20 @@
 using namespace std;
 int main(){
 
-//   printf("Hello India\n");
 int i,j,n;
+char ch =65;
 cout<<"Enter the number"<<endl;
 cin>>n;
 for ( i = 1; i<=n; i++)
 {
-    for ( j = 1; j<=i; j++)
+    for ( j = 1; j<=n; j++)
     {
-        cout<<i-j+1;
+        cout<<ch;
+
     }
     cout<<endl;
+     ch++;
 }
+
 
 }
