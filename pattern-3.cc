f99877f20bf2cb873843f60093a27637e6587963
aknasa <aknasa12@gmail.com>
2022-01-31 10:09:04 +0530

    P-4

--- pattern-3.cc
+++ pattern-3.cc
@@ -1,26 +1,21 @@
 #include<iostream>
 using namespace std;
 int main(){
-
-
-#include <iostream>
-using namespace std;
-int main()
-{
-    //   A B C
-    //   A B C
-    //   A B C
+ 
+    //   A 
+    //   B B 
+    //   C C C
     int i, j, n;
     char ch = 65;
     cout << "Enter the number" << endl;
     cin >> n;
     for (i = 1; i <= n; i++)
     {  
-        for (j= 1; j<=n; j++)
+        for (j= 1; j<=i; j++)
         {
             cout<<ch;
-            ch++;
         }
+            ch++;
         cout<<endl;
     }
 
@@ -28,6 +23,6 @@ int main()
 
 
     
-}
+
 
 
