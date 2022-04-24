dbe6d4f5d7d7040fc6dc5d7d6fffd40fa4292739
Ankit  Kumar <aknasa12@gmail.com>
2022-04-24 21:29:04 +0530

    Add files via upload

---
+++ Dynamic_M.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+
+int getSum(int *arr, int n){
+   
+   int sum=0;
+   for(int i=0; i<n; i++){
+    sum+= arr[i];
+
+
+   }
+return sum;
+
+}
+ 
+int main() {
+
+int n;
+cin>>n;
+
+// Variable size Array  
+int* arr = new int[n];
+
+
+// taken array from the user as a input  
+for(int i=0;i<n;i++){
+
+    cin>>arr[i];
+}
+
+int ans =getSum(arr,n);
+ cout <<ans<<endl;
+
+
+
+
+
+}
